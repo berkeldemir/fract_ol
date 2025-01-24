9f94bc45329a232815f0eca71ff9d040df286fc3
berkeldemir <berkeldemir@outlook.com>
2025-01-24 23:11:49 +0300

    aaaaaaaaaaaaaaaaaa

--- fractol.h
+++ fractol.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fractol.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: beldemir <beldemir@42istanbul.com.tr>      +#+  +:+       +#+        */
+/*   By: beldemir <beldemir@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 18:51:33 by beldemir          #+#    #+#             */
-/*   Updated: 2025/01/24 16:44:37 by beldemir         ###   ########.fr       */
+/*   Updated: 2025/01/24 22:52:39 by beldemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <stdarg.h>
 # include <fcntl.h>
 # include <math.h>
-# include "./mlx/mlx.h"
+# include "./mlx-mac/mlx.h"
 # include <stdio.h>
 
 typedef struct s_app
--- main.c
+++ main.c
@@ -3,31 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: beldemir <beldemir@42istanbul.com.tr>      +#+  +:+       +#+        */
+/*   By: beldemir <beldemir@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 18:48:53 by beldemir          #+#    #+#             */
-/*   Updated: 2025/01/24 16:46:10 by beldemir         ###   ########.fr       */
+/*   Updated: 2025/01/24 23:07:47 by beldemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int	key_hook(int i, t_app *app)
-{
-	printf("Hello from key_hook!\n");
-	return (0);
-}
+//int	key_hook(int i, t_app *app)
+//{
+//	printf("Hello from key_hook!\n");
+//	return (0);
+//}
 
 int main(void)
 {
-    t_app *app;
+    void    *mlx;
+    void    *win;
 
-    app->mlx = mlx_init();
-    app->win = mlx_new_window(app->mlx, 800, 600, "Minilibx Penceresi");
-    if (!app->win || !app->mlx)
+    mlx = mlx_init();
+    win = mlx_new_window(mlx, 800, 600, "Minilibx Penceresi");
+    if (!win || !mlx)
         return (1);
 
-    mlx_key_hook(app->win, key_hook, &app);
-    mlx_loop(app->mlx);
+    //mlx_key_hook(app->win, key_hook, &app);
+    mlx_loop(mlx);
     return (0);
 }
